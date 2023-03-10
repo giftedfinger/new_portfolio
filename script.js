db9eee1dcde367395d2484b213f745c432561902
giftedfinger <olakunle.iyiola@yahoo.com>
2023-03-10 16:24:26 +0100

    some adjustment on the header, footer, slills and about

--- script.js
+++ script.js
@@ -30,33 +30,6 @@ const dotContainer = document.querySelector('.dots');
 
 
 
-// Modal window
-
-const openModal = (e)=> {
-    console.log('prevent')
-    e.preventDefault();
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-};
-
-const closeModal = ()=> {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-};
-
-btnsOpenModal.forEach(btn=> btn.addEventListener('click', openModal))
-
-// for (let i = 0; i < btnsOpenModal.length; i++)
-//     btnsOpenModal[i].addEventListener('click', openModal);
-
-// btnCloseModal.addEventListener('click', closeModal);
-// overlay.addEventListener('click', closeModal);
-
-document.addEventListener('keydown', (e)=> {
-    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-        closeModal();
-    }
-});
 
 //////////////////////////////////////////////////////////
 // Smooth Scrolling
